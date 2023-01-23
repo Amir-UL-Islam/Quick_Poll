724aadbe8a263e090949572d9474074541658575
Amir-UL <mangoleo51@gmail.com>
2023-01-23 21:59:44 +0600

    Security level 01 (authentication and authorization done)

--- src/main/java/org/amir/pollat/Security/Quick_PollUserDetailsService.java
+++ src/main/java/org/amir/pollat/Security/Quick_PollUserDetailsService.java
@@ -1,29 +1,21 @@
-//package org.amir.pollat.Security;
-//
-//import org.amir.pollat.entity.Users;
-//import org.amir.pollat.repository.UsersRepository;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Component;
-//
-//import javax.inject.Inject;
-//import java.util.ArrayList;
-//
-//@Component
-//public abstract class Quick_PollUserDetailsService implements UserDetailsService {
-//    @Inject
-//    private UsersRepository usersRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        Users users =  usersRepository.findByUsername(username);
-//        if (users == null) {
-//            throw new UsernameNotFoundException(String.format("User with the Username %s Dose not found", username));
-//        }
-//        // Create a granted authority based on user's role.
-//// Can't pass null authorities to user. Hence initialize with an empty arraylist
-//        UserDetails userDetails = new org.springframework.security.core.userdetails.User(users.getUsername(), users.getPassword(), new ArrayList<>());
-//        return userDetails;
-//    }
-//}
+package org.amir.pollat.Security;
+
+import org.amir.pollat.entity.Users;
+import org.amir.pollat.repository.UsersRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+
+@Service
+public class Quick_PollUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("amir", "amir", new ArrayList<>());
+    }
+}
